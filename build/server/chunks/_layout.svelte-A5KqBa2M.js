39df7159d22c36b1be8a63448b3dc117b6e562d8
Javad <80436401+Javadk-ir@users.noreply.github.com>
2024-06-25 18:50:59 +0330

    Delete build directory

--- build/server/chunks/_layout.svelte-A5KqBa2M.js
+++
@@ -1,8 +0,0 @@
-import { c as create_ssr_component } from './ssr-PqYfa7Lv.js';
-
-const Layout = create_ssr_component(($$result, $$props, $$bindings, slots) => {
-  return `<div class="app"><main>${slots.default ? slots.default({}) : ``}</main></div>`;
-});
-
-export { Layout as default };
-//# sourceMappingURL=_layout.svelte-A5KqBa2M.js.map
--- build/server/chunks/hooks.server-ZCRjAl4Z.js
+++
@@ -1,7 +0,0 @@
-import { s as start_mongo } from './mongo-n06qetVz.js';
-import 'mongodb';
-
-start_mongo().then(() => {
-  console.log("Mongo Started successfully");
-});
-//# sourceMappingURL=hooks.server-ZCRjAl4Z.js.map
